24bc806f73d574ef7799d5f22d75acf5b84a7dae
USER <lk.suntasigr@uea.edu.ec>
2024-02-27 18:18:51 -0500

    arreglos

--- Arreglo_ multimedia.py
+++ Arreglo_ multimedia.py
@@ -1,3 +1,26 @@
-# ordenar en forma acendente
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+def ordenar_fila_en_matriz(matriz, fila):
+    fila_ordenada = list(matriz[fila])
+    bubble_sort(fila_ordenada)
+    matriz[fila] = fila_ordenada
+# Matriz de ejemplo (3x3)
+matriz = [
+    [3, 1, 5],
+    [4, 2, 7],
+    [9, 6, 8]
+]
+print("Matriz original:")
+for fila in matriz:
+    print(fila)
+fila_a_ordenar = 0
+ordenar_fila_en_matriz(matriz, fila_a_ordenar)
+print("\nMatriz con la fila", fila_a_ordenar, "ordenada:")
+for fila in matriz:
+    print(fila)
 
 
