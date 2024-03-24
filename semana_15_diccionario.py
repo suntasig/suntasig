c89d7f5316be1e9cf0be1f05847542a316de1fa7
USER <lk.suntasigr@uea.edu.ec>
2024-03-23 21:07:37 -0500

    semana_15

---
+++ semana_15_diccionario.py
@@ -0,0 +1,24 @@
+# crear un diccionario con información ficticia
+
+información_personal = {
+    "nombre": "Luis Suntasig",
+    "edad":35,
+    "ciudad":"Latacunga"
+}
+
+# Acceder al valor asociado con la clave ciudad y nodificarlo
+información_personal["ciudad"] = "Quito"
+
+# Agregar una nueva clave para presentar la profeción
+información_personal["profeción"] = "Arquitecto"
+
+# verificar numero telefonico si existe y agregar si no existe
+if "telefono" not in información_personal:
+    información_personal["telefono"] = "0987853470"
+
+# Eliminar la clave de edad
+información_personal.pop("edad", None)
+
+# imprimir el diccionario final
+print("diccionario final")
+print(información_personal)
