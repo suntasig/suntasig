bd07152f0a760d728d9f9c042ced81e787fcca40
USER <lk.suntasigr@uea.edu.ec>
2024-06-07 22:20:29 -0500

    semana_16

--- my_notes.txt.py
+++ my_notes.txt.py
@@ -1,8 +1,10 @@
 # Crear el archivo my_notes.txt y escribir algunas notas
 with open("my_notes.txt", "w") as file:
-    file.write("1. Recordar comprar leche en el supermercado.\n")
-    file.write("2. Llamar a Juan para confirmar la cita.\n")
-    file.write("3. Terminar el informe antes del viernes.\n")
+    file.write("1. Comprar carne, huevos, papas, arroz.\n")
+    file.write("2. Recordar comprar leche en el supermercado.\n")
+    file.write("3. Llamar a Juan para confirmar la cita.\n")
+    file.write("4. Terminar el informe antes del viernes.\n")
+    file.write("5. Guardar  informe final.\n")
 
 # Abrir el archivo y leer las notas línea por línea
 with open("my_notes.txt", "r") as file:
